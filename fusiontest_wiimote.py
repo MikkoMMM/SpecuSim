c1965718913f849304eb9645a07ce9737208f819
Mikko Merikivi <mikkommm@gmail.com>
2021-01-11 20:14:26 +0200

    Wiimote calibration tool

--- fusiontest_wiimote.py
+++ fusiontest_wiimote.py
@@ -32,7 +32,7 @@ while True:
     # This ordering of values seems to correspond most closely to the order yaw, pitch, and then roll. Though, the "yaw" doesn't give intuitive values.
     # Tilting the Wiimote upward in pitch, the ['acc'][1] gets lower. Tilting it down, the ['acc'][1] gets higher.
     # Rolling it counterclockwise, the ['acc'][0] gets lower. Rolling clockwise, the ['acc'][0] value gets higher.
-    accel = (wiimote.state['acc'][0]-122, wiimote.state['acc'][1]-122.9, wiimote.state['acc'][2]-122.5)
+    accel = (wiimote.state['acc'][0]-123.15, wiimote.state['acc'][1]-123.06, wiimote.state['acc'][2]-123.1)
 
     angle_rates = wiimote.state['motionplus']['angle_rate']
 
@@ -48,9 +48,9 @@ while True:
 
     # The -7945, -8115 and -7964 calibrate the values so that for me the gyro values will be close to zero when the Wii Remote is resting on the table.
     gyro = (
-        (angle_rates[0]-7945)/(4.0+wiimote.state['motionplus']['low_speed'][0]*16),
-        (angle_rates[1]-8115)/(4.0+wiimote.state['motionplus']['low_speed'][1]*16),
-        (angle_rates[2]-7964)/(4.0+wiimote.state['motionplus']['low_speed'][2]*16),
+        (angle_rates[0]-8119.34)/(4.0+wiimote.state['motionplus']['low_speed'][0]*16),
+        (angle_rates[1]-7943.68)/(4.0+wiimote.state['motionplus']['low_speed'][1]*16),
+        (angle_rates[2]-7959.64)/(4.0+wiimote.state['motionplus']['low_speed'][2]*16),
     )
     fuse.update_nomag(accel, gyro, time.time())
     deltag2 = deltat(time.time()) * radians(gyro[2])
--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from direct.gui.DirectGui import DirectFrame
 
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
+from direct.stdpy import thread
 
 def timediff(time1, time2):
     return (time1-time2)
@@ -28,7 +29,7 @@ class MyApp(ShowBase):
         # Apply scale and position transforms on the model.
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(-8, 42, 0)
-        self.camera.setPos(0, -10, 3)
+        self.camera.setPos(0, -3, 3)
 
         # For calculating motion controller orientation
         self.heading = 0
@@ -45,7 +46,42 @@ class MyApp(ShowBase):
         self.wiimote.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC | cwiid.RPT_MOTIONPLUS | cwiid.RPT_IR
         self.wiimote.enable(cwiid.FLAG_MOTIONPLUS)
         self.wiimote.led = 1
+        print("Wiimote connected")
 
+        connectWiiText['text'] = "Place the Wiimote on a flat surface."
+        base.graphicsEngine.renderFrame()
+        base.graphicsEngine.renderFrame()
+        base.graphicsEngine.syncFrame()
+        
+        print()
+        print("Starting calibration")
+        while True:
+            time.sleep(0.01)
+            self.gyrobias = [0,0,0]
+            self.accbias = [0,0,0]
+            if 'motionplus' not in self.wiimote.state:
+                continue
+            def inner():
+                for i in range(100):
+                    for j in range(0,3):
+                        self.gyrobias[j] = (self.gyrobias[j]*i + self.wiimote.state['motionplus']['angle_rate'][j]) / (i+1)
+                        self.accbias[j] = (self.accbias[j]*i + self.wiimote.state['acc'][j]) / (i+1)
+                        if abs(self.gyrobias[j] - self.wiimote.state['motionplus']['angle_rate'][j]) >= 20.0:
+                            print("Retrying. Reason: gyro ", j, ". ", i, " iterations.")
+                            return
+                        if abs(self.accbias[j] - self.wiimote.state['acc'][j]) > 1:
+                            print("Retrying. Reason: accelerometer ", j, ". ", i, " iterations.")
+                            return False
+                    time.sleep(0.01)
+                print("Successful calibration")
+                return True
+            if inner():
+                break
+
+        self.accbias[2] = (self.accbias[0] + self.accbias[1]) / 2 # It's difficult to subtract gravity, so let's just assume a bias
+        self.accbias[0] += 0.005 # To avoid a potential division by zero
+        print("Gyro biases: ", self.gyrobias)
+        print("Accelerometer biases: ", self.accbias)
         connectWiiText.destroy()
 
         # Tasks that are repeated ad infinitum
@@ -57,9 +93,9 @@ class MyApp(ShowBase):
         if 'motionplus' in self.wiimote.state:
             angle_rates = self.wiimote.state['motionplus']['angle_rate']
             gyro = (
-                (angle_rates[0]-7945)/(4.0+self.wiimote.state['motionplus']['low_speed'][0]*16),
-                (angle_rates[1]-8115)/(4.0+self.wiimote.state['motionplus']['low_speed'][1]*16),
-                (angle_rates[2]-7964)/(4.0+self.wiimote.state['motionplus']['low_speed'][2]*16),
+                (angle_rates[0]-self.gyrobias[0])/(4.0+self.wiimote.state['motionplus']['low_speed'][0]*16),
+                (angle_rates[1]-self.gyrobias[1])/(4.0+self.wiimote.state['motionplus']['low_speed'][1]*16),
+                (angle_rates[2]-self.gyrobias[2])/(4.0+self.wiimote.state['motionplus']['low_speed'][2]*16),
             )
         else:
             gyro = (0,0,0)
@@ -77,7 +113,7 @@ class MyApp(ShowBase):
 
     # Define a procedure to move the camera.
     def spinCameraTask(self, task):
-        self.camera.setPos(0, -10, 3)
+        self.camera.setPos(0, -3, 3)
         self.camera.setHpr(self.heading, -self.fuse.roll, self.fuse.pitch)
         return Task.cont
 
